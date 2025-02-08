25fe9e79a12e009d0cc72da344b6fdf519ef315b
Teja Dasari <ichorlifephoenix@gmail.com>
2025-02-07 23:25:50 -0800

    May change #3; changing body of the page.

--- bro-code-tutorials/src/Button.jsx
+++ bro-code-tutorials/src/Button.jsx
@@ -1,20 +1,11 @@
 function Button() {
 
-    let count = 0;
+    const handleClick = (e) => {
+        console.log(e);
+        e.target.textContent = "I was clicked!";
+    }
 
-    const handleClick = (name) => {
-        if (count < 3) {
-            count++;
-            console.log(`Hello ${name}! You clicked me ${count} times!`);
-        }
-        else { 
-            count++;
-            console.log(`You clicked me ${count} times!`);
-        }
-    };
-    //const handleClick2 = (name) => console.log(`Hello ${name}!`);
-
-  return <button onClick={() => handleClick("Teja Dasari")}>Click me!</button>;
+    return <button onClick={(e) => handleClick(e)}>Click me!</button>;
 }
 
 export default Button;
