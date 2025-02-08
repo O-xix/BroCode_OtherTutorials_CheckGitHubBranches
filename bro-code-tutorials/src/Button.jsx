120b408c4e3c471a241a90d0dad9c88ece5f1887
Teja Dasari <ichorlifephoenix@gmail.com>
2025-02-07 23:17:32 -0800

    May change #2; changing case in function.

--- bro-code-tutorials/src/Button.jsx
+++ bro-code-tutorials/src/Button.jsx
@@ -1,9 +1,20 @@
 function Button() {
 
-    const handleClick = () => console.log('Button clicked!');
+    let count = 0;
+
+    const handleClick = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`Hello ${name}! You clicked me ${count} times!`);
+        }
+        else { 
+            count++;
+            console.log(`You clicked me ${count} times!`);
+        }
+    };
     //const handleClick2 = (name) => console.log(`Hello ${name}!`);
 
-  return <button onClick={() => handleClick2("Teja Dasari")}>Click me!</button>;
+  return <button onClick={() => handleClick("Teja Dasari")}>Click me!</button>;
 }
 
 export default Button;
